54a5664233a7f233e8a584ba59e543b3bee93dce
Roberto Monteiro <beto@MacBook-Pro-de-Mac.local>
2023-05-29 14:43:18 -0300

    :tada: First Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  // alert("oi")
+  const html = document.documentElement
+
+  // html.classList.toogle("light")
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar-dark.png")
+  }
+}
